ee1271dc15cd3e96924f95286f5c7e4e0e3a9a2e
Vuja98 <vujacicdejan98@gmail.com>
2023-02-10 02:11:50 +0100

    Fixed problem with Fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,11 +1,12 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 // console.log(Fraction);
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
   _message = '';
+  Fraction = require('fractional').Fraction;
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
