e87f94355530311360408bf8ef00c91a4dbe9c55
Vuja98 <vujacicdejan98@gmail.com>
2023-02-10 01:25:28 +0100

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarksHandler);
   recipeView.addHandlerRender(controlRecipes);
@@ -126,6 +129,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
+  newFeature();
 };
 init();
